94f010ac4e23cd2596aa924dd9c853135d168479
Hastingamo <hezekiahhastings14@gmail.com>
2025-11-23 16:06:01 +0100

    yyyyy

--- my-app/src/app/Wallet/page.js
+++ my-app/src/app/Wallet/page.js
@@ -1,17 +1,17 @@
-"use client";
-import React from 'react'
-import ConnectButton from '../Component/ConnectButton';
-// import { Web3Provider } from '../Component/Waa'
-// import { ConnectKitButton } from 'connectkit';
-function Page() {
-  return (
-    <div>
-        <h1> wallet </h1>
-      {/* <ConnectKitButton/> */}
-      <ConnectButton/>
-  </div>
-  )
-}
+// "use client";
+// import React from 'react'
+// import ConnectButton from '../Component/ConnectButton';
+// // import { Web3Provider } from '../Component/Waa'
+// // import { ConnectKitButton } from 'connectkit';
+// function Page() {
+//   return (
+//     <div>
+//         <h1> wallet </h1>
+//       {/* <ConnectKitButton/> */}
+//       <ConnectButton/>
+//   </div>
+//   )
+// }
 
 
-export default Page
+// export default Page
